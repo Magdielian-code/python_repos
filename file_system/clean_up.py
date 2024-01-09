484e29fde35e12142c5313c0eaa9e578305cb8b9
Magdielian-code <oziomaagaecheta81@gmail.com>
2024-01-09 18:00:52 +0100

    modified clean_up.py

--- file_system/clean_up.py
+++ file_system/clean_up.py
@@ -11,4 +11,50 @@ f()
 try:
     f()
 except Exception as e:
-    print(f"caught {type(e)}: e")
+    print(f"caught {type(e)}: e"
+def f():
+    raise ExceptionGroup(
+        "group1",
+        [
+            OSError(1),
+            SystemError(2),
+            ExceptionGroup(
+                "group2",
+                [
+                    OSError(3),
+                    RecursionError(4)
+                ]
+            )
+        ]
+    )
+
+try:
+    f()
+except* OSError as e:
+    print("There were OSErrors")
+except* SystemError as e:
+    print("There were SystemErrors")
+
+tests = [1,2,3,4,]
+excs = []
+for test in tests:
+    try:
+        test.run()
+    except Exception as e:
+        excs.append(e)
+
+if excs:
+   raise ExceptionGroup("Test Failures", excs)
+
+
+
+# Enriching exceptions with notes
+
+try:
+    raise TypeError('bad type')
+except Exception as e:
+    e.add_note('Add some information')
+    e.add_note('Add some more information')
+    raise
+
+)
