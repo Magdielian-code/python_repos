3427f279ad17aca08ee7dde21623efd95b7df03b
Magdielian-code <oziomaagaecheta81@gmail.com>
2024-01-09 17:24:37 +0100

    modified read_write.py and created workfile.txt

--- file_system/read_write.py
+++ file_system/read_write.py
@@ -1,10 +1,27 @@
-f = open('workfile', 'w', encoding="utf-8")
-
-with open('workfile', encoding="utf-8") as f:
+with open('workfile.txt', 'r', encoding="utf-8") as f:
     read_data = f.read()
 
 # we can check that the file is autmatically closed.
-f.closed
-f.close()
-f.open('workfile', encoding="utf-8")
-f.read()
+print(read_data)
+
+f.readline()
+f.readline()
+f.readline()
+
+for line in f:
+    print(line, end='')
+
+f.write("This is a test\n")
+
+
+value = ('the answer', 42)
+s = str(value)  # convert the tuple to string
+f.write(s)
+
+
+f = open('workfile', 'rb+')
+f.write(b'0123456789abcdef')
+f.seek(5)      # Go to the 6th byte in the file
+f.read(1)
+f.seek(-3, 2)  # Go to the 3rd byte before the end
+f.read(1)
