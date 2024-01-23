6dede3cb3034e267daf523ef6d6c0d2d5f3c351d
Magdielian-code <oziomaagaecheta81@gmail.com>
2024-01-23 10:40:44 +0100

    modofied HomeShift/main.py

--- HomeShift/main.py
+++ HomeShift/main.py
@@ -1,13 +1,13 @@
-# # Importing modules and packages
-# import ShiftItem_Ozi.books
-# import ShiftItem_Ozi.Clothes.jeans
+# Importing modules and packages
+import ShiftItem_Ozi.books
+import ShiftItem_Ozi.Clothes.jeans
 
-# ShiftItem_Ozi.Clothes.jeans.display()
-# print()
+ShiftItem_Ozi.Clothes.jeans.display()
+print()
 
-# # How to call a mclass and class module in a package
-# b = ShiftItem_Ozi.books.MyClass()
-# b.bookType()
+# How to call a mclass and class module in a package
+b = ShiftItem_Ozi.books.MyClass()
+b.bookType()
 
 from ShiftItem_Ozi import books
 from ShiftItem_Ozi.Footwears import shoes, boots
